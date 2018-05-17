891d1b059400c10de761b2ebec8eb9c7107d28f1
ballardcz <ballardcz@gmail.com>
2018-05-17 15:55:37 -0700

    trying to fix this twitter problem

--- liri.js
+++ liri.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
-var keys = require("./keys");
-var Twitter = require("twitter");
-var Spotify = require("node-spotify-api");
+var requireKeys = require("./keys");
+var requireTwitter = require("twitter");
+var requireSpotify = require("node-spotify-api");
 var request = require("request");
 var fs = require("fs");
 //console.log(keys);
 
 //var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var client = new Twitter(requireKeys.twitter);
 
 // capture action from command line
 var action = process.argv[2];
@@ -21,91 +21,87 @@ switch (action) {
   case 'my-tweets':
     Twitter();
     break;
-  case 'Spotify-this-song':
+  case 'spotify-this-song':
     Spotify();
     break;
   case 'movie-this':
     Ombd();
     break;
   default:
-    console.log("You didn't put in a proper command, Use 'my-tweets', 'Spotify', or 'Ombd'!");
+    console.log("You didn't put in a proper command, Use 'my-tweets', 'spotify-this-song', or 'movie-this'!");
     break;
-}
+};
 
 
 function Twitter() {
-
+  console.log('this hit');
   //This will show your last 20 tweets and when they were created at in your terminal/bash window.
   var params = {
     screen_name: "ballardcz1"
   };
+//statuses/user_timeline"
+  client.get("favorites/list", params, function(error, tweets, response) {
+    console.log(tweets);
 
-  client.get("statuses/user_timeline", params, function (error, tweets, response) {
-console.log(tweets.text);
-      for (var i = 0; i > tweets; i++) {
-        console.log(tweets[i].text);
-        console.log('=================================');
+    if (error) throw error;
 
-        //   if (error) {
-        //     console.log(error);
-        //   } else {
-
-        //     for (var i = 0; i < tweets; i ++) {
-        //     console.log('=================================');
-        //     console.log(tweets.text);
-        //     console.log('=================================');
-        //     console.log(tweets[i].created_at);
-        //     // console.log(tweets[i].);  
-        //     console.log('=================================');
-        //     //  console.log(response); 
-        //   }
-        // }
-      }});
-  }
-
-
-  function Ombd() {
-
-    var movie = process.argv[3];
-    // movie = [];
-
-    if (movie !== "") {
-
-  //     * Title of the movie.
-  // * Year the movie came out.
-  // * IMDB Rating of the movie.
-  // * Rotten Tomatoes Rating of the movie.
-  // * Country where the movie was produced.
-  // * Language of the movie.
-  // * Plot of the movie.
-  // * Actors in the movie.
-  // * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-      var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function (res) {
-        //console.log(res);
-        console.log(res.title);
-      //  console.log(res.)
-        
-      })
-    } else {
-      console.log("Hey put in a movie title!")
+    for (var i = 0; i < tweets; i++) {
+      console.log(tweets[i].text);
+      console.log('=================================');
+      //     console.log(tweets[i].created_at);  
+      //     console.log('=================================');
+      console.log(response);
     }
-  }
+  });
+};
 
-  function Spotify() {
-    // This will show the following information about the song in your terminal/bash window
 
-    // Artist(s)
-    
-    // The song's name
-    
-    // A preview link of the song from Spotify
-    
-    // The album that the song is from
-    
-    // If no song is provided then your program will default to "The Sign" by Ace of Base.
+function Ombd() {
+
+  var movie = process.argv[3];
+  // movie = [];
+
+  if (movie != "") {
+    // var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    //     * Title of the movie.
+    // * Year the movie came out.
+    // * IMDB Rating of the movie.
+    // * Rotten Tomatoes Rating of the movie.
+    // * Country where the movie was produced.
+    // * Language of the movie.
+    // * Plot of the movie.
+    // * Actors in the movie.
+    // * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+    // $.ajax({
+    //   url: queryURL,
+    //   method: "GET"
+    // }).then(function(res) {
+    //   //console.log(res);
+    //   console.log(res.title);
+    //    console.log(res.year);
+    //    console.log(res.rating);
+    // });
+    request("https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log(response);
+      console.log('body:', body); // Print the HTML for the Google homepage.
+    });
+
+  } else {
+    console.log("Hey put in a movie title!")
   }
+};
+
+function Spotify() {
+  // This will show the following information about the song in your terminal/bash window
+
+  // Artist(s)
+
+  // The song's name
+
+  // A preview link of the song from Spotify
+
+  // The album that the song is from
+
+  // If no song is provided then your program will default to "The Sign" by Ace of Base.
+};
